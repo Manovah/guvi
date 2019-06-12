0a4f5fd71dc8bbaca33c8868a6e465dc55756c7e
Manovah <51685947+Manovah@users.noreply.github.com>
2019-06-12 20:15:30 +0530

    Update largestnum.py

--- code kata/largestnum.py
+++ code kata/largestnum.py
@@ -1,13 +1,13 @@
-x=int(input())
-y=int(input())
-z=int(input())
-if (x>y):
-  if(x>z):
-    print(x)
+m=int(input())
+n=int(input())
+o=int(input())
+if (m>n):
+  if(m>o):
+    print(m)
   else:
-    print(z)
+    print(o)
 else:
-  if(y>z):
-    print(y)
+  if(n>o):
+    print(n)
   else:
-    print(z)
+    print(o)
